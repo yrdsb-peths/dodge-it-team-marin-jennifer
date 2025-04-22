c9eea4446c1618fbd294515731358e34a49c0312
marin-yan <marinxyan@gmail.com>
2025-04-22 08:44:23 -0400

    initialized hero

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,8 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Hero penguin = new Hero();
+        addObject(penguin, 100, 100);
     }
 }
