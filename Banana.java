76365bcf849876eabed3b8b276a5e2f3fc5b0f05
JenniferTuWu <jenniferydt@gmail.com>
2025-04-22 09:10:18 -0400

    Banana + Bomb

---
+++ Banana.java
@@ -0,0 +1,30 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Banana extends Actor
+{
+    public void act()
+    {
+        move(-30);
+        
+        if(getX() <= 0) {
+            resetBanana();
+        }
+        
+        if(isTouching(Hero.class)){
+            //add a game over symbol
+            Bomb bomb = new Bomb();
+            getWorld().addObject(bomb, 300, 200);
+            getWorld().removeObject(this);
+        }
+    }
+    
+    public void resetBanana(){
+        int num = Greenfoot.getRandomNumber(2);
+        if(num == 0){
+            setLocation(600, 100);
+        }
+        else{
+            setLocation(600, 300);
+        }
+    }
+}
